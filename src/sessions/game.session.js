d5ebed171b62c4b036161835d6cd0fab6d10cac3
doughnutdough5 <doughnutdough5@gmail.com>
2024-12-19 00:01:55 +0900

    fix: 주석 해제 및 addGameSession에서 raw 객체를 복원시키게 변경
    Co-authored-by: Park Junho <bigsmile1460@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>

--- src/sessions/game.session.js
+++ src/sessions/game.session.js
@@ -1,47 +1,63 @@
-// import Game from '../classes/model/game.class.js';
-// import { gameSession } from './session.js';
+import Game from '../classes/model/game.class.js';
+import User from '../classes/model/user.class.js';
+import CharacterData from '../classes/model/characterData.class.js';
+import { plainToInstance } from 'class-transformer';
+import { gameSession } from './session.js';
+import { addUser } from './user.session.js';
 
-// export const addGameSession = (gameId, ownerId, name, maxUserNum) => {
-//   const session = new Game(gameId, ownerId, name, maxUserNum);
-//   gameSession.push(session);
-//   return session;
-// };
+// 새 게임 세션 대신 기존 json형태의 게임 객체를 복원하고 넣어주는 역할할
+// roomData << raw 객체체
+export const addGameSession = (roomData) => {
+    let users = [];
+    roomData.users.forEach((user) => {
+      user.characterData.handCards = new Map(Object.entries(user.characterData.handCards));
+      user.characterData = plainToInstance(CharacterData, user.characterData);
+      const newUser = plainToInstance(User, user);
+      users.push(newUser);
+      addUser(newUser);
+    });
+    const room = plainToInstance(Game, roomData);
+    room.users = users;
 
-// export const removeGameSession = (gameId) => {
-//   const index = gameSession.findIndex((game) => game.id === gameId);
-//   // 못 찾은 경우
-//   if (index === -1) {
-//     console.error('게임을 찾지 못했습니다.');
-//     return null;
-//   }
+  gameSession.push(room);
+  return room;
+};
 
-//   const game = gameSession.splice(index, 1)[0];
-//   if (game) {
-//     game.release();
-//   }
+export const removeGameSession = (gameId) => {
+  const index = gameSession.findIndex((game) => game.id === gameId);
+  // 못 찾은 경우
+  if (index === -1) {
+    console.error('게임을 찾지 못했습니다.');
+    return null;
+  }
 
-//   return game;
-// };
+  const game = gameSession.splice(index, 1)[0];
+  if (game) {
+    game.release();
+  }
 
-// export const findGameById = (gameId) => {
-//   const index = gameSession.findIndex((game) => game.id === gameId);
-//   if (index !== -1) {
-//     return gameSession[index];
-//   }
-// };
+  return game;
+};
 
-// export const joinGameSession = (gameId, user) => {
-//   const index = gameSession.findIndex((game) => game.id === gameId);
-//   // 못 찾은 경우
-//   if (index === -1) {
-//     console.error('게임을 찾지 못했습니다.');
-//     return null;
-//   }
-//   gameSession[index].users.push(user);
+export const findGameById = (gameId) => {
+  const index = gameSession.findIndex((game) => game.id === gameId);
+  if (index !== -1) {
+    return gameSession[index];
+  }
+};
 
-//   return gameSession[index];
-// };
+export const joinGameSession = (gameId, user) => {
+  const index = gameSession.findIndex((game) => game.id === gameId);
+  // 못 찾은 경우
+  if (index === -1) {
+    console.error('게임을 찾지 못했습니다.');
+    return null;
+  }
+  gameSession[index].users.push(user);
 
-// export const getAllGameSessions = () => {
-//   return gameSession;
-// };
+  return gameSession[index];
+};
+
+export const getAllGameSessions = () => {
+  return gameSession;
+};
