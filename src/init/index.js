6bfc7adf5c52c40469188424e102a41aa25644b5
doughnutdough5 <doughnutdough5@gmail.com>
2024-12-19 23:10:10 +0900

    fix: charaterPositions를 서버가 켜질 때 불러오게 변경
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Park Junho <bigsmile1460@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/init/index.js
+++ src/init/index.js
@@ -1,12 +1,12 @@
 import RedisManager from '../classes/manager/redis.manager.js';
-import { gameStartHandler } from '../handler/room/gameStart.handler.js';
+import { setCharacterPositions } from '../constants/characterPositions.js';
 import { loadProto } from './loadProtos.js';
 
 const initServer = async () => {
   try {
     await loadProto();
+    setCharacterPositions();
     const redis = RedisManager.getInstance();
-
     await redis.subscribe('lobby');
     //유저 정보 등록
     const parsedMessage = redis.onGameStart();
