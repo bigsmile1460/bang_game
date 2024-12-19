7b5d31002c954cc9fd821eb738bfad9304e8160a
doughnutdough5 <doughnutdough5@gmail.com>
2024-12-19 23:10:58 +0900

    fix: addUser를 healthCheckHandler에서 하게 변경
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Park Junho <bigsmile1460@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/sessions/game.session.js
+++ src/sessions/game.session.js
@@ -3,7 +3,6 @@ import User from '../classes/model/user.class.js';
 import CharacterData from '../classes/model/characterData.class.js';
 import { plainToInstance } from 'class-transformer';
 import { gameSession } from './session.js';
-import { addUser } from './user.session.js';
 
 // 새 게임 세션 대신 기존 json형태의 게임 객체를 복원하고 넣어주는 역할할
 // roomData << raw 객체체
@@ -13,8 +12,8 @@ export const addGameSession = (roomData) => {
       user.characterData.handCards = new Map(Object.entries(user.characterData.handCards));
       user.characterData = plainToInstance(CharacterData, user.characterData);
       const newUser = plainToInstance(User, user);
+      console.log(newUser.handCards);
       users.push(newUser);
-      addUser(newUser);
     });
     const room = plainToInstance(Game, roomData);
     room.users = users;
