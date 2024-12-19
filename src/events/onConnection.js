c7eaf8d1ce6f97a09b259b9fa6e9db8d9bd706c9
doughnutdough5 <doughnutdough5@gmail.com>
2024-12-19 23:08:42 +0900

    fix: onConnection 정상화화
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: Park Junho <bigsmile1460@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -1,31 +1,11 @@
-import jwt from 'jsonwebtoken';
-import RedisManager from '../classes/manager/redis.manager.js';
-import { config } from '../config/config.js';
 import { onData } from './onData.js';
 import { onEnd } from './onEnd.js';
 import { onError } from './onError.js';
-import { findUserByJwt } from '../sessions/user.session.js';
-
-// export const onConnection = (socket) => {
-//   console.log(`Client connected from ${socket.remoteAddress}:${socket.remotePort}`);
-
-//   socket.buffer = Buffer.alloc(0);
-
-//   socket.on('data', onData(socket));
-//   socket.on('end', onEnd(socket));
-//   socket.on('error', onError(socket));
-// };
 
 export const onConnection = async (socket) => {
   console.log(`Client connected from ${socket.remoteAddress}:${socket.remotePort}`);
 
   socket.buffer = Buffer.alloc(0);
-  // JWT 생성 (IP와 포트를 사용하여 JWT 생성)
-
-  // const jwtToken = jwt.sign({ id:`${socket.remoteAddress}:${socket.remotePort}` }, config.jwt.SCRET_KEY,{noTimestamp:true});
-  // socket.jwt = jwtToken;
-  // const currentUser = findUserByJwt(jwtToken);
-  // currentUser.socket = socket; 
 
   socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
