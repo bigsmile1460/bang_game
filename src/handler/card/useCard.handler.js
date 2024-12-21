bfe42f2604faa9f20d0d27c4b1edcb1016a29513
bigsmile1460 <wns4711@naver.com>
2024-12-21 15:34:22 +0900

    fix: 기능 주석 해제
    Co-authored-by: sdh02142 <sdh02142@users.noreply.github.com>
    Co-authored-by: doughnutdough5 <doughnutdough5@users.noreply.github.com>
    Co-authored-by: Freak-J <Freak-J@users.noreply.github.com>
    Co-authored-by: LEE, DONGEUN <armsterd@users.noreply.github.com>

--- src/handler/card/useCard.handler.js
+++ src/handler/card/useCard.handler.js
@@ -1,82 +1,85 @@
-// import { PACKET_TYPE } from '../../constants/header.js';
-// import { Packets } from '../../init/loadProtos.js';
-// import { findGameById } from '../../sessions/game.session.js';
-// import { getUserBySocket } from '../../sessions/user.session.js';
-// import useCardNotification from '../../utils/notification/useCard.notification.js';
-// import userUpdateNotification from '../../utils/notification/userUpdate.notification.js';
-// import { createResponse } from '../../utils/response/createResponse.js';
-// import getCardHandlerByCardType from './index.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+import { Packets } from '../../init/loadProtos.js';
+import { findGameById } from '../../sessions/game.session.js';
+import { getUserBySocket } from '../../sessions/user.session.js';
+import useCardNotification from '../../utils/notification/useCard.notification.js';
+import userUpdateNotification from '../../utils/notification/userUpdate.notification.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+import getCardHandlerByCardType from './index.js';
 
-// export const useCardHandler = (socket, payload) => {
-//   try {
-//     const useCardType = payload.useCardRequest.cardType; //사용 카드
-//     const targetUserId = payload.useCardRequest.targetUserId.low; //대상자 ID
-//     const cardUsingUser = getUserBySocket(socket); //카드 사용자
-//     const currentGame = findGameById(cardUsingUser.roomId);
-//     const targetUser = currentGame.findInGameUserById(targetUserId);
+export const useCardHandler = (socket, payload) => {
+  try {
+    const useCardType = payload.useCardRequest.cardType; //사용 카드
+    const targetUserId = payload.useCardRequest.targetUserId.low; //대상자 ID
+    const cardUsingUser = getUserBySocket(socket); //카드 사용자
+    const currentGame = findGameById(cardUsingUser.roomId);
+    const targetUser = currentGame.findInGameUserById(targetUserId);
 
-//     // 페이즈가 밤이면 에러 리스폰스 반환하기(밤에 카드 사용 막기)
-//     if (currentGame.currentPhase === Packets.PhaseType.END) {
-//       const errorPayload = {
-//         useCardResponse: {
-//           success: false,
-//           failCode: Packets.GlobalFailCode.INVALID_PHASE,
-//         },
-//       };
+    console.l;
+    console.log('[카드사용] 대상유저:', targetUser);
 
-//       socket.write(createResponse(PACKET_TYPE.USE_CARD_RESPONSE, 0, errorPayload));
-//       userUpdateNotification(currentGame.users);
+    // 페이즈가 밤이면 에러 리스폰스 반환하기(밤에 카드 사용 막기)
+    if (currentGame.currentPhase === Packets.PhaseType.END) {
+      const errorPayload = {
+        useCardResponse: {
+          success: false,
+          failCode: Packets.GlobalFailCode.INVALID_PHASE,
+        },
+      };
 
-//       return;
-//     }
+      socket.write(createResponse(PACKET_TYPE.USE_CARD_RESPONSE, 0, errorPayload));
+      userUpdateNotification(currentGame.users);
 
-//     const cardHandler = getCardHandlerByCardType(useCardType);
-//     if (!cardHandler) {
-//       console.error('카드 핸들러를 찾을 수 없습니다.');
-//       return;
-//     }
+      return;
+    }
 
-//     // 에러 안나면 아무것도 반환하지 않기
-//     const errorResponse = cardHandler(cardUsingUser, targetUser, currentGame, useCardType);
-//     if (errorResponse) {
-//       // 뭔가 에러가 났음.
-//       console.error('카드 핸들러: 뭔가 문제 있음');
-//       socket.write(createResponse(PACKET_TYPE.USE_CARD_RESPONSE, 0, errorResponse));
-//       return;
-//     }
+    const cardHandler = getCardHandlerByCardType(useCardType);
+    if (!cardHandler) {
+      console.error('카드 핸들러를 찾을 수 없습니다.');
+      return;
+    }
 
-//     // 일반카드1~12 무조건 덱으로,총 13~16, 장비 17~20, 디버프 21~23 - 사라질 때 덱으로
-//     cardUsingUser.removeHandCard(useCardType); // 카드 사용자의 손에 들고 있던 카드 제거
-//     if (useCardType < 13) {
-//       currentGame.returnCardToDeck(useCardType); // 카드 덱으로 복귀
-//     }
+    // 에러 안나면 아무것도 반환하지 않기
+    const errorResponse = cardHandler(cardUsingUser, targetUser, currentGame, useCardType);
+    if (errorResponse) {
+      // 뭔가 에러가 났음.
+      console.error('카드 핸들러: 뭔가 문제 있음');
+      socket.write(createResponse(PACKET_TYPE.USE_CARD_RESPONSE, 0, errorResponse));
+      return;
+    }
 
-//     const useCardNotificationResponse = useCardNotification(
-//       useCardType,
-//       cardUsingUser.id,
-//       targetUserId,
-//     );
+    // 일반카드1~12 무조건 덱으로,총 13~16, 장비 17~20, 디버프 21~23 - 사라질 때 덱으로
+    cardUsingUser.removeHandCard(useCardType); // 카드 사용자의 손에 들고 있던 카드 제거
+    if (useCardType < 13) {
+      currentGame.returnCardToDeck(useCardType); // 카드 덱으로 복귀
+    }
 
-//     //게임 방 안에 모든 유저들에게 카드 사용알림
-//     currentGame.users.forEach((user) => {
-//       user.socket.write(
-//         createResponse(PACKET_TYPE.USE_CARD_NOTIFICATION, 0, useCardNotificationResponse),
-//       );
-//     });
+    const useCardNotificationResponse = useCardNotification(
+      useCardType,
+      cardUsingUser.id,
+      targetUserId,
+    );
 
-//     // 동기화
-//     userUpdateNotification(currentGame.users);
+    //게임 방 안에 모든 유저들에게 카드 사용알림
+    currentGame.users.forEach((user) => {
+      user.socket.write(
+        createResponse(PACKET_TYPE.USE_CARD_NOTIFICATION, 0, useCardNotificationResponse),
+      );
+    });
 
-//     // 성공 response 전송
-//     const responsePayload = {
-//       useCardResponse: {
-//         success: true,
-//         failCode: Packets.GlobalFailCode.NONE_FAILCODE,
-//       },
-//     };
+    // 동기화
+    userUpdateNotification(currentGame.users);
 
-//     socket.write(createResponse(PACKET_TYPE.USE_CARD_RESPONSE, 0, responsePayload));
-//   } catch (e) {
-//     console.error(e);
-//   }
-// };
+    // 성공 response 전송
+    const responsePayload = {
+      useCardResponse: {
+        success: true,
+        failCode: Packets.GlobalFailCode.NONE_FAILCODE,
+      },
+    };
+
+    socket.write(createResponse(PACKET_TYPE.USE_CARD_RESPONSE, 0, responsePayload));
+  } catch (e) {
+    console.error(e);
+  }
+};
--- src/handler/index.js
+++ src/handler/index.js
@@ -2,23 +2,23 @@ import { PACKET_TYPE } from '../constants/header.js';
 import CustomError from '../utils/error/customError.js';
 import { gameStartHandler } from './room/gameStart.handler.js';
 import { healthCheckHandler } from './healthCheck.handler.js';
-// import { useCardHandler } from './card/useCard.handler.js';
-// import { positionUpdateHandler } from './game/positionUpdate.handler.js';
-// import { reactionHandler } from './game/reaction.handler.js';
-// import { destroyCardHandler } from './card/destroyCard.handler.js';
-// import { fleaMarketPickHandler } from './game/fleaMarketPick.handler.js';
-// import { cardSelectHandler } from './card/cardSelect.handler.js';
-// import { passDebuffHandler } from './game/passDebuff.handler.js';
+import { useCardHandler } from './card/useCard.handler.js';
+import { positionUpdateHandler } from './game/positionUpdate.handler.js';
+import { reactionHandler } from './game/reaction.handler.js';
+import { destroyCardHandler } from './card/destroyCard.handler.js';
+import { fleaMarketPickHandler } from './game/fleaMarketPick.handler.js';
+import { cardSelectHandler } from './card/cardSelect.handler.js';
+import { passDebuffHandler } from './game/passDebuff.handler.js';
 
 const handlers = {
   [PACKET_TYPE.GAME_START_REQUEST]: { handler: gameStartHandler },
-  // [PACKET_TYPE.USE_CARD_REQUEST]: { handler: useCardHandler },
-  // [PACKET_TYPE.POSITION_UPDATE_REQUEST]: { handler: positionUpdateHandler },
-  // [PACKET_TYPE.REACTION_REQUEST]: { handler: reactionHandler },
-  // [PACKET_TYPE.DESTROY_CARD_REQUEST]: { handler: destroyCardHandler },
-  // [PACKET_TYPE.FLEA_MARKET_PICK_REQUEST]: { handler: fleaMarketPickHandler },
-  // [PACKET_TYPE.CARD_SELECT_REQUEST]: { handler: cardSelectHandler },
-  // [PACKET_TYPE.PASS_DEBUFF_REQUEST]: { handler: passDebuffHandler },
+  [PACKET_TYPE.USE_CARD_REQUEST]: { handler: useCardHandler },
+  [PACKET_TYPE.POSITION_UPDATE_REQUEST]: { handler: positionUpdateHandler },
+  [PACKET_TYPE.REACTION_REQUEST]: { handler: reactionHandler },
+  [PACKET_TYPE.DESTROY_CARD_REQUEST]: { handler: destroyCardHandler },
+  [PACKET_TYPE.FLEA_MARKET_PICK_REQUEST]: { handler: fleaMarketPickHandler },
+  [PACKET_TYPE.CARD_SELECT_REQUEST]: { handler: cardSelectHandler },
+  [PACKET_TYPE.PASS_DEBUFF_REQUEST]: { handler: passDebuffHandler },
   [PACKET_TYPE.HEALTHCHECK_REQUEST]: {handler: healthCheckHandler}
 };
 
